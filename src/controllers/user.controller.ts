cf35f09b64b7bcd8e0d2135bec23ae159bc7141d
hardzal <suryadijogja@gmail.com>
2025-02-16 21:31:57 +0700

    add user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import userService from '../services/user.service';
-import { createUserSchema } from '../utils/schemas/user.schema';
+import {
+  createUserSchema,
+  updateUserSchema,
+} from '../utils/schemas/user.schema';
 
 class UserController {
   async getUsers(req: Request, res: Response) {
@@ -30,6 +33,47 @@ class UserController {
       return res.json(user);
     } catch (error) {
       return res.json(error);
+    }
+  }
+
+  async updateUserById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+
+      const user = await userService.getUserById(id);
+
+      if (!user) {
+        res.status(404).json({
+          messagE: 'user not found',
+        });
+        return;
+      }
+
+      const { email, username } = await updateUserSchema.validateAsync(body);
+
+      if (email != '') {
+        user.email = email;
+      }
+
+      if (username != '') {
+        user.username = username;
+      }
+      const updateUser = await userService.updateUserById(id, user);
+      res.json(updateUser);
+    } catch (error) {
+      res.json(error);
+    }
+  }
+
+  async deleteUserById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const user = await userService.deleteUserById(id);
+      res.json(user);
+    } catch (error) {
+      res.json(error);
     }
   }
 }
