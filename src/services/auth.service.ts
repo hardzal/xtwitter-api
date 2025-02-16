705882f054e0e78d99e43f1abf6e4843608d88df
hardzal <suryadijogja@gmail.com>
2025-02-16 22:53:15 +0700

    Add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { RegisterDTO } from '../dtos/auth.dto';
+import { prisma } from '../libs/prisma';
+
+class AuthService {
+  async register(data: RegisterDTO) {
+    const { fullName, ...userData } = data;
+
+    return await prisma.user.create({
+      data: {
+        ...userData,
+        profile: {
+          create: {
+            fullName,
+          },
+        },
+      },
+    });
+  }
+
+  async resetPassword(email: string, hashedNewPassword: string) {
+    return await prisma.user.update({
+      where: { email },
+      data: {
+        password: hashedNewPassword,
+      },
+    });
+  }
+}
+
+export default new AuthService();
