0d040e49908b30c57817edb5d51471bf9ed397f1
hardzal <suryadijogja@gmail.com>
2025-02-20 02:16:20 +0700

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import rootRouter from './routes/root.route';
 import apiRouter from './routes/api.route';
 import swaggerUI from 'swagger-ui-express';
 import swaggerDoc from '../swagger/swagger-output.json';
+import cors from 'cors';
 
 dotenv.config();
 //
@@ -12,8 +13,37 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // npm run swagger to generate new api points
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://b59-circle.vercel.app'],
+  })
+);
+app.use(express.json());
+app.use(
+  '/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDoc, {
+    customSiteTitle: 'Circle App API',
+    customfavIcon: 'NONE',
+    isExplorer: true,
+    customJs:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customCss: `
+              .swagger-ui .topbar { display: none } 
+              .information-container.wrapper { background:rgb(0, 255, 60); padding: 2rem } 
+              .information-container .info { margin: 0 } 
+              .information-container .info .main { margin: 0 !important} 
+              .information-container .info .main .title { color:rgb(0, 0, 0)} 
+              .renderedMarkdown p { margin: 0 !important; color:rgb(0, 0, 0) !important }
+              `,
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  })
+);
 app.use(rootRouter);
 app.use('/api', apiRouter);
 
