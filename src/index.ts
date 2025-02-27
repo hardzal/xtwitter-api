35d83966dc9e83a66815c0028b8911e5d24bf553
hardzal <suryadijogja@gmail.com>
2025-02-28 02:35:00 +0700

    Add vercel.json

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://circle-project-five.vercel.app'],
   })
 );
 app.use(express.json());
