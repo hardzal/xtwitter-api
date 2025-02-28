89f7d13462aba7f0664e5dd257ae5151170c395e
hardzal <suryadijogja@gmail.com>
2025-02-28 14:21:21 +0700

    update link cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://circle-project-five.vercel.app'],
+    origin: ['http://localhost:5173', 'https://circle-project-five.vercel.app'],
   })
 );
 app.use(express.json());
